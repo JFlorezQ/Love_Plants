9fb52cb8aeb9e2e2ea4bafa4968e48cfd55da614
JFlorezQ <juli.florez@gmail.com>
2025-05-07 14:59:30 -0500

    banners

--- client/src/Components/Banners/Banners.jsx
+++ client/src/Components/Banners/Banners.jsx
@@ -1,28 +1,36 @@
 function Banners() {
   return (
-    <div className="m-4  p-8 max-w-full overflow-hidden">
-      <div className="flex flex-wrap mb-4 justify-center items-center">
+    <div className="m-4 p-8 max-w-full overflow-hidden">
+      {/* Banner 1 */}
+      <div className="flex justify-center items-center mb-4">
         <img
           src="/banner1.png"
           alt="banner1"
-          className="flex max-h-[auto] w-full pl-20 pr-20 mr-2 mb-2"
+          className="w-full max-h-[30rem] object-cover"
         />
       </div>
 
-      <div className="flex flex-wrap mb-4 justify-center items-center pl-20 pr-20">
-        <img
-          src="/banner2.png"
-          alt="banner2"
-          className="flex max-h-[50rem] w-1/2 mb-2"
-        />
-        <img
-          src="./banner3.png"
-          alt=" banner3"
-          className="flex max-h-[50rem] w-1/2 mb-2"
-        />
+      {/* Banners 2 y 3 */}
+      <div className="flex justify-center items-stretch gap-4 px-20">
+        <div className="w-1/2 h-[30rem] overflow-hidden">
+          <img
+            src="/banner2.png"
+            alt="banner2"
+            className="w-full h-full object-cover"
+          />
+        </div>
+        <div className="w-1/2 h-[30rem] overflow-hidden">
+          <img
+            src="/banner3.png"
+            alt="banner3"
+            className="w-full h-full object-cover"
+          />
+        </div>
       </div>
     </div>
   );
 }
+
+
 
 export default Banners;
