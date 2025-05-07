af4ca152989f2253d19bff2c5d8f551fdc9f69a8
JFlorezQ <juli.florez@gmail.com>
2025-05-07 13:07:55 -0500

    seguridad

--- server/src/server.js
+++ server/src/server.js
@@ -3,7 +3,15 @@ const morgan = require("morgan");
 const routes = require("./routes/index.js");
 const session = require("express-session");
 const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const pgSession = require("connect-pg-simple")(session);
+const { Pool } = require("pg");
 const server = express();
+
+const pool = new Pool({
+  connectionString: process.env.DB_URL,
+  ssl: { rejectUnauthorized: false },
+}); 
 
 // Middleware de registro de solicitudes (morgan)
 server.use(morgan("dev"));
@@ -32,12 +40,16 @@ server.use((req, res, next) => {
 // Middleware para sesiones
 server.use(
   session({
-    secret: "secreto",
+    store: new pgSession({
+      pool: pool,
+      tableName: "session", // puedes cambiar el nombre si quieres
+    }),
+    secret: process.env.SESSION_SECRET, // ponlo en un env en producción
     resave: false,
-    saveUninitialized: true,
+    saveUninitialized: false,
     cookie: {
-      secure: true, // Cambia a true si estás usando HTTPS
-      maxAge: 24 * 60 * 60 * 1000, // Duración de la sesión en milisegundos (1 día en este caso)
+      secure: process.env.NODE_ENV === "production", // true solo si estás en HTTPS
+      maxAge: 24 * 60 * 60 * 1000,
     },
   })
 );
