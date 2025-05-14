ad80519b972916bf9e570c8211f4ddbf95aae78d
JFlorezQ <juli.florez@gmail.com>
2025-05-14 08:39:57 -0500

    algunso cambios de estilo y comentarios create products

--- client/src/Components/DashboardComponents/CreateProduct/CreateProduct.jsx
+++ client/src/Components/DashboardComponents/CreateProduct/CreateProduct.jsx
@@ -206,250 +206,153 @@ function CreateProduct() {
   };
 
   return (
-    <div className="max-w-md mx-auto pb-10 mt-[-3rem]">
-      <NavbarAdmin />
-      <h1 className="text-2xl font-bold mb-8 flex items-center justify-center">
-        Crear Producto
-      </h1>
-      <form className="border border-chiliRed rounded p- text-arial text-base flex-col flex items-center  justify-center ">
-        <div>
-          <div>
-            <label className="block text-chiliRed mb-2">
-              Nombre del producto: <span classname ="text-red">*</span>
-            </label>
-            <input
-              type="text"
-              name="name"
-              placeholder="Ingrese su nombre"
-              value={input.name}
-              onChange={handleChange}
-              className="border rounded p-3 w-full bg-whiteSmoke focus:outline-none"
-            />
-          </div>
-          <div>
-            <label className="block text-chiliRed mb-2">Descripción:<span classname ="text-red">*</span> </label>
-            <input
-              type="text"
-              name="description"
-              placeholder="Ingrese la descripción"
-              value={input.description}
-              onChange={handleChange}
-              className="border rounded p-3 w-full bg-whiteSmoke focus:outline-none"
-            />
-          </div>
-          <div>
-            <label className="block text-chiliRed mb-2">Precio: <span classname ="text-red">*</span></label>
-            <input
-              type="number"
-              name="price"
-              placeholder="Ingrese el precio"
-              value={input.price}
-              min={0}
-              onChange={handleChange}
-              className="border rounded p-3 w-full bg-whiteSmoke focus:outline-none"
-            />
-          </div>
-          <div>
-            <label className="block text-chiliRed mb-2">Descuento: <span classname ="text-red">*</span></label>
-            <div className="flex items-center mt-2 mb-2">
-              <input
-                type="number"
-                name="discount"
-                placeholder="Ingrese el descuento"
-                value={input.discount}
-                onChange={handleChange}
-                className="border rounded p-3 w-full bg-whiteSmoke focus:outline-none"
-                min="0"
-                max="100"
-              />
-              <span className="ml-2">%</span>
-            </div>
-          </div>
-          <div>
-            <label className="block text-chiliRed mb-2">
-              Especificaciones: <span classname ="text-red">*</span>
-            </label>
-            <input
-              type="text"
-              name="specifications"
-              placeholder="Ingrese cada especificación"
-              value={input.specifications}
-              onChange={handleChange}
-              className="border rounded p-3 w-full bg-whiteSmoke focus:outline-none"
-            />
-          </div>
-          <div>
-            <label className="block text-chiliRed mb-2">Stock: <span classname ="text-red">*</span></label>
-            <input
-              type="number"
-              name="stock"
-              placeholder="Ingrese la cantidad de productos"
-              value={input.stock}
-              min={0}
-              onChange={handleChange}
-              className="border rounded p-3 w-full bg-whiteSmoke focus:outline-none"
-            />
-          </div>
-          <div>
-            <label className="block text-chiliRed mb-2">Imagen: <span classname ="text-red">*</span></label>
-            <input
-              type="file"
-              accept="image/*"
-              name="imageFile"
-              multiple
-              onChange={handleImageChange}
-              className="border rounded p-3 w-full bg-whiteSmoke focus:outline-none"
-            />
-          </div>
+   // Contenedor principal centrado con ancho medio y padding inferior
+<div className="max-w-md mx-auto pb-10 mt-[-3rem]">
+  {/* Barra de navegación del admin */}
+  <NavbarAdmin />
 
-          <div>
-            <label className="block text-chiliRed mb-2">
-              Cantidad de imágenes seleccionadas:
-            </label>
-            <p>{imageFile?.length}</p>
-          </div>
-          <div>
-            <label className="block text-chiliRed mb-2">Color: <span classname ="text-red">*</span></label>
-            <div className="flex items-center mb-2">
-              <select
-                name="color"
-                placeholder="Selecciona el color"
-                onChange={(event) => handleSelect(event)}
-                className="border rounded p-3 w-full bg-whiteSmoke focus:outline-none"
-              >
-                <option value="color">Seleccione color</option>
-                {color?.map((element) => (
-                  <option value={element.id} key={element.id}>
-                    {element.color}
-                  </option>
-                ))}
-              </select>
-              <button
-                type="button"
-                onClick={handleNewColorClick}
-                className="bg-chiliRed text-whiteSmoke py-2 px-4 rounded-full ml-2 hover:scale-105 hover:shadow-xl transition duration-100 ease-in-out"
-              >
-                {showNewColorInput ? "-" : "+"}
-              </button>
-            </div>
-            {showNewColorInput && (
-              <div>
-                <input
-                  type="text"
-                  name="newColorInput"
-                  placeholder="Ingrese nuevo Color"
-                  value={newColorInput}
-                  onChange={(e) => setNewColorInput(e.target.value)}
-                  className="border rounded p-2 bg-whiteSmoke focus:outline-none"
-                />
-                <button
-                  onClick={addColor}
-                  className="bg-chiliRed text-whiteSmoke p-1 m-2 rounded hover:scale-105 hover:shadow-xl transition duration-100 ease-in-out"
-                >
-                  Agregar
-                </button>
-              </div>
-            )}
-          </div>
-          <div>
-            <label className="block text-chiliRed mb-2">Marca: <span classname ="text-red">*</span></label>
-            <div className="flex items-center mb-2">
-              <select
-                name="brand"
-                placeholder="Selecciona la marca"
-                onChange={(event) => handleSelect(event)}
-                className="border rounded p-3 w-full bg-whiteSmoke focus:outline-none"
-              >
-                <option value="brand">Seleccione marca</option>
-                {brands &&
-                  brands.map((element) => (
-                    <option value={element.id} key={element.id}>
-                      {element.brand}
-                    </option>
-                  ))}
-              </select>
-              <button
-                type="button"
-                onClick={handleNewBrandClick}
-                className="bg-chiliRed text-whiteSmoke py-2 px-4 rounded-full ml-2 hover:scale-105 hover:shadow-xl transition duration-100 ease-in-out"
-              >
-                {showNewBrandInput ? "-" : "+"}
-              </button>
-            </div>
-            {showNewBrandInput && (
-              <div>
-                <input
-                  type="text"
-                  name="newBrandInput"
-                  placeholder="Ingrese nueva Marca"
-                  value={newBrandInput}
-                  onChange={(e) => setNewBrandInput(e.target.value)}
-                  className="border rounded p-2 bg-whiteSmoke focus:outline-none"
-                />
-                <button
-                  onClick={addBrand}
-                  className="bg-chiliRed text-whiteSmoke p-1 m-2 rounded hover:scale-105 hover:shadow-xl transition duration-100 ease-in-out"
-                >
-                  Agregar
-                </button>
-              </div>
-            )}
-          </div>
-          <div>
-            <label className="block text-chiliRed mb-2">Categoria: <span classname ="text-red">*</span></label>
-            <div className="flex items-center mb-2">
-              <select
-                name="category"
-                placeholder="Selecciona la categoria"
-                onChange={(event) => handleSelect(event)}
-                className="border rounded p-3 w-full bg-whiteSmoke focus:outline-none"
-              >
-                <option value="category">Seleccione una categoria</option>
-                {categories?.map((element) => (
-                  <option value={element.id} key={element.id}>
-                    {element.category}
-                  </option>
-                ))}
-              </select>
-              <button
-                type="button"
-                onClick={handleNewCategoryClick}
-                className="bg-chiliRed text-whiteSmoke py-2 px-4 rounded-full ml-2 hover:scale-105 hover:shadow-xl transition duration-100 ease-in-out"
-              >
-                {showNewCategoryInput ? "-" : "+"}
-              </button>
-            </div>
-            {showNewCategoryInput && (
-              <div>
-                <input
-                  type="text"
-                  name="newCategoryInput"
-                  placeholder="Ingrese nueva categoría"
-                  value={newCategoryInput}
-                  onChange={(e) => setNewCategoryInput(e.target.value)}
-                  className="border rounded p-2 bg-whiteSmoke focus:outline-none"
-                />
-                <button
-                  onClick={addCategory}
-                  className="bg-chiliRed text-whiteSmoke p-1 m-2 rounded hover:scale-105 hover:shadow-xl transition duration-100 ease-in-out"
-                >
-                  Agregar
-                </button>
-              </div>
-            )}
-          </div>
+  {/* Título de la sección */}
+  <h1 className="text-2xl font-bold mb-8 flex items-center justify-center text-PrimaryColor">
+    Crear Producto
+  </h1>
+
+  {/* Formulario de creación de producto */}
+  <form className="border border-PrimaryColor rounded p-6 text-base flex-col flex items-center justify-center bg-White text-Black">
+    <div>
+      {/* Campo: Nombre del producto */}
+      <div>
+        <label className="block text-PrimaryColor mb-2">
+          Nombre del producto: <span className="text-AccentColor">*</span>
+        </label>
+        <input
+          type="text"
+          name="name"
+          placeholder="Ingrese su nombre"
+          value={input.name}
+          onChange={handleChange}
+          className="border rounded p-3 w-full bg-WhiteSmoke focus:outline-none"
+        />
+      </div>
+
+      {/* Campo: Descripción */}
+      <div>
+        <label className="block text-PrimaryColor mb-2">
+          Descripción: <span className="text-AccentColor">*</span>
+        </label>
+        <input
+          type="text"
+          name="description"
+          placeholder="Ingrese la descripción"
+          value={input.description}
+          onChange={handleChange}
+          className="border rounded p-3 w-full bg-WhiteSmoke focus:outline-none"
+        />
+      </div>
+
+      {/* Campo: Precio */}
+      <div>
+        <label className="block text-PrimaryColor mb-2">
+          Precio: <span className="text-AccentColor">*</span>
+        </label>
+        <input
+          type="number"
+          name="price"
+          placeholder="Ingrese el precio"
+          value={input.price}
+          min={0}
+          onChange={handleChange}
+          className="border rounded p-3 w-full bg-WhiteSmoke focus:outline-none"
+        />
+      </div>
+
+      {/* Campo: Descuento */}
+      <div>
+        <label className="block text-PrimaryColor mb-2">
+          Descuento: <span className="text-AccentColor">*</span>
+        </label>
+        <div className="flex items-center mt-2 mb-2">
+          <input
+            type="number"
+            name="discount"
+            placeholder="Ingrese el descuento"
+            value={input.discount}
+            onChange={handleChange}
+            className="border rounded p-3 w-full bg-WhiteSmoke focus:outline-none"
+            min="0"
+            max="100"
+          />
+          <span className="ml-2">%</span>
         </div>
-        <div className="mt-4 mb-4">
-          <button
-            type="submit"
-            className="bg-chiliRed text-whiteSmoke py-3 px-6 rounded-full w-full"
-            onClick={handleSumit}
-          >
-            Guardar Producto
-          </button>
-        </div>
-      </form>
+      </div>
+
+      {/* Campo: Especificaciones */}
+      <div>
+        <label className="block text-PrimaryColor mb-2">
+          Especificaciones: <span className="text-AccentColor">*</span>
+        </label>
+        <input
+          type="text"
+          name="specifications"
+          placeholder="Ingrese cada especificación"
+          value={input.specifications}
+          onChange={handleChange}
+          className="border rounded p-3 w-full bg-WhiteSmoke focus:outline-none"
+        />
+      </div>
+
+      {/* Campo: Stock */}
+      <div>
+        <label className="block text-PrimaryColor mb-2">
+          Stock: <span className="text-AccentColor">*</span>
+        </label>
+        <input
+          type="number"
+          name="stock"
+          placeholder="Ingrese la cantidad de productos"
+          value={input.stock}
+          min={0}
+          onChange={handleChange}
+          className="border rounded p-3 w-full bg-WhiteSmoke focus:outline-none"
+        />
+      </div>
+
+      {/* Campo: Imágenes */}
+      <div>
+        <label className="block text-PrimaryColor mb-2">
+          Imagen: <span className="text-AccentColor">*</span>
+        </label>
+        <input
+          type="file"
+          accept="image/*"
+          name="imageFile"
+          multiple
+          onChange={handleImageChange}
+          className="border rounded p-3 w-full bg-WhiteSmoke focus:outline-none"
+        />
+      </div>
+
+      {/* Visualización del número de imágenes seleccionadas */}
+      <div>
+        <label className="block text-PrimaryColor mb-2">
+          Cantidad de imágenes seleccionadas:
+        </label>
+        <p>{imageFile?.length}</p>
+      </div>
     </div>
+
+    {/* Botón de envío */}
+    <div className="mt-4 mb-4">
+      <button
+        type="submit"
+        className="bg-PrimaryColor text-White py-3 px-6 rounded-full w-full hover:bg-AccentColor transition-colors duration-300"
+        onClick={handleSumit}
+      >
+        Guardar Producto
+      </button>
+    </div>
+  </form>
+</div>
+
   );
 }
 export default CreateProduct;
